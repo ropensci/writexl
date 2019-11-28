b9eb024988e4be481512d367d77a833216814c51
Jeroen Ooms <jeroenooms@gmail.com>
2019-11-28 15:24:32 +0100

    Fix warning about timezone attribute getting lost

--- tests/testthat/test-performance.R
+++ tests/testthat/test-performance.R
@@ -4,5 +4,6 @@ test_that("Performance is OK", {
   tmp <- writexl::write_xlsx(nycflights13::flights)
   out <- readxl::read_xlsx(tmp)
   unlink(tmp)
+  attr(out$time_hour, 'tzone') <- attr(nycflights13::flights$time_hour, 'tzone')
   expect_equal(out, nycflights13::flights)
 })
