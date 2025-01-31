1b99e0c6913f62f759867c0c21d52dcc5e398c57
Jeroen Ooms <jeroenooms@gmail.com>
2025-01-31 16:02:05 +0100

    Use S3 to invoke as.double on bit64

--- R/write_xlsx.R
+++ R/write_xlsx.R
@@ -62,7 +62,7 @@ normalize_df <- function(df){
   }
   for(i in which(vapply(df, inherits, logical(1), "integer64"))){
     warning(sprintf("Coercing columnn %s from int64 to double", names(df)[i]), call. = FALSE)
-    df[[i]] <- bit64::as.double.integer64(df[[i]])
+    df[[i]] <- as.double(df[[i]])
   }
   df
 }
